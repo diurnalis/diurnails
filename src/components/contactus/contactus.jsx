40ec9f5c5749d0ae30de98e89d51853aca72a98d
Rinku2002 <gangishettyrinku@gmail.com>
2022-05-20 19:40:11 +0530

    Added aos effect and css for text

--- src/components/contactus/contactus.jsx
+++ src/components/contactus/contactus.jsx
@@ -1,24 +1,31 @@
+import { useEffect } from "react";
 import "./contactus.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFacebook, faInstagram, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons';
 import logo from "../../images/logo.jpeg"
 
+import Aos from 'aos'
+import "aos/dist/aos.css"
+
 function Contactus() {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
-        <div>
+        <div className="w-100">
             <div className="bg pb-4">
-                <img src={logo} alt="logo" className="w-25 rounded rounded-circle"/>
-                <h3 className="text-center text-dark bg-white container">Contact Us</h3>
-                <p className="text-center container text-dark bg-white fw-bolder">Have a question? You can find a host of brilliant articles in our 
+                <img data-aos="zoom-in" src={logo} alt="logo" className="w-25 rounded rounded-circle mb-3 mt-3"/>
+                <h3 data-aos="zoom-in" className="text-center text-dark bg-white container">Contact Us</h3>
+                <p data-aos="zoom-in" className="text-center container text-dark bg-white fw-bolder">Have a question? You can find a host of brilliant articles in our 
                     knowledge base. If you can't find what you're looking for, we'd be happy to point
                     you in the right direction. Please feel free to send us an email or even give us a
                     call at 123456789
                 </p>
             </div>
 
-            <div className='row mt-5 justify-content-center bg-light'>
-                <div className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
-                    <h4>H and S department</h4>
+            <div className='row mt-5 justify-content-center bg-light w-100 mx-auto'>
+                <div data-aos="fade-left" className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
+                    <h3>H and S department</h3>
                     <p>Vignana Jyothi Nagar,</p>
                     <p>Pragati Nagar,</p>
                     <p>Nizampet(S.O),</p>
@@ -27,7 +34,7 @@ function Contactus() {
                     <br />
                 </div>
                 
-                <div className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
+                <div data-aos="fade-down" className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
                     <h4 className='fw-bolder'>Contact information:</h4>
                     <h2>General Inquires</h2>
                     <p className="text-primary">diurnalis.vnrvjiet@gmail.com</p><br/>
@@ -38,7 +45,7 @@ function Contactus() {
                     
                 </div>
 
-                <div className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
+                <div data-aos="fade-right" className='col col-lg-4 col-md-4 col-sm-12 col-12 border p-5'>
                     <h4 className='fw-bolder'>Connect with us on social media</h4><br/>
 
                     <FontAwesomeIcon icon={faTwitter} className="display-6" /> <a className="h4" href="#"> Twitter</a><br/>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import "./home.css";
 import logo from "../../images/logo.jpeg";
 import { useEffect } from "react";
+import news from "../../images/news.gif"
 
 import Aos from 'aos'
 import "aos/dist/aos.css"
@@ -11,19 +12,24 @@ function Home() {
     }, []);
 
     return (
-        <div>
-            <div className="row justify-content-center mt-5">
-                <img data-aos="slide-right" className="w-25 col-lg-6 col-sm-10 d-block mx-auto border border-5 rounded rounded-circle" src={logo} alt="logo" />
-                <div data-aos="slide-left" className="col-lg-6 col-sm-10 mx-auto my-auto">
+        <div className="mt-5">
+            <div className="row justify-content-center w-100">
+                <img data-aos="slide-right" className="w-25 col-lg-5 col-sm-10 d-block mx-auto border border-5 rounded rounded-circle" src={logo} alt="logo" />
+                <div data-aos="slide-left" className="col-lg-5 col-sm-10 mx-auto my-auto">
                     <h1>Journalism club of VNRVJIET</h1>
                 </div>
             </div>
 
             <div>
-                Times of VNR is an emerging newspaper club of VNRVJIET. We are a team of students,
-                aiming to circulate news from student sources across our college.
-                Every aspect would be taken care of, as our team consists of exceptional scrutinizers.
-                Even you too can contribute articles. More details would be out soon.
+                {/* 
+                <img data-aos="zoom-in-up" src={news} className="w-50" />
+                */}
+                <h2 data-aos="zoom-in-up" className="container fw-bolder text-success mt-5">
+                    Times of VNR is an emerging newspaper club of VNRVJIET. We are a team of students,
+                    aiming to circulate news from student sources across our college.
+                    Every aspect would be taken care of, as our team consists of exceptional scrutinizers.
+                    Even you too can contribute articles. More details would be out soon.
+                </h2>
             </div>
         </div>
     );
